1c6a7e990a87b0195578b68e700b54a1d37f413d
Gauravsingh096 <singhgaurav07122004@gmail.com>
2025-04-03 18:36:30 +0530

    added env

--- app.py
+++ app.py
@@ -114,6 +114,19 @@ import logging
 import uuid
 from asgiref.wsgi import WsgiToAsgi
 
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load .env variables
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+
+
+
 # Initialize FastAPI app
 app = FastAPI()
 app.add_middleware(
